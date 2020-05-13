7abbc714f22e51b05269edfe283e0bddc824b7e3
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2020-05-13 05:29:45 +0200

    initial Data Images+Names+Colors

---
+++ Splits/Splits/Images+Names+Colors.swift
@@ -0,0 +1,26 @@
+//
+//  Images+Names+Colors.swift
+//  Splits
+//
+//  Created by Islam Gharib on 5/13/20.
+//  Copyright © 2020 Gharib. All rights reserved.
+//
+
+import UIKit
+
+
+// Images
+let camera = UIImage(named: "1")
+let city = UIImage(named: "2")
+let buffalo = UIImage(named: "3")
+let bouquet = UIImage(named: "4")
+let stand = UIImage(named: "5")
+let urban = UIImage(named: "6")
+
+let imageArray = [camera,city,buffalo,bouquet,stand,urban]
+
+// Names
+let namesArray = ["CAMERA","CITY","ANIMALS","FLOWERS","STANDS","URBAN"]
+
+// Colors
+let colorsArray = [#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 1, green: 0.7580876015, blue: 0.1015545004, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)]
